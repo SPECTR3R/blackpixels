a0edef0759f126ec24330abfe69c9829073d0f01
Armando del Río <adrio.coder@gmail.com>
2021-11-01 08:54:19 -0600

    Fix server call

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,18 +1,21 @@
 import logo from './logo.svg'
 import { useState } from 'react'
 import './App.css'
-import { MatrixSize } from './proto/blackPixels_pb'
+import { MatrixSize, MatrixValues } from './proto/blackPixels_pb'
 import { PixelServiceClient } from './proto/BlackPixelsServiceClientPb'
 import Canvas from './components/Canvas'
 
 function App() {
+  const client = new PixelServiceClient('http://localhost:8080')
+
+  const [size, setSize] = useState<number>(3)
+
   const [matrix, setMatrix] = useState([
     [1, 0, 0],
     [0, 1, 0],
     [0, 0, 1]
   ])
   // useEffect(() => {
-  //   const client = new PixelServiceClient('http://localhost:8080')
   //   const matrixSize = new MatrixSize()
   //   matrixSize.setN(5)
   //   matrixSize.setN(5)
@@ -24,6 +27,28 @@ function App() {
   //     console.log('aweso')
   //   })
   // }, [])
+
+  const getMatrix = async () => {
+    const theMatrix: any = []
+    const matrixSize = new MatrixSize()
+    matrixSize.setN(size)
+    matrixSize.setM(size)
+    const stream1 = client.getMatrix(matrixSize)
+    stream1.on('data', chunk => {
+      const msg = (chunk as MatrixValues).toObject()
+      console.log(msg.listList)
+      theMatrix.push(msg.listList)
+    })
+    stream1.on('end', () => {
+      setMatrix(() => theMatrix)
+    })
+  }
+  const updateMatrix = async () => {
+    // const matrixSize = new MatrixSize()
+    // matrixSize.setN(5)
+    // matrixSize.setN(5)
+    // const stream1 = client.getMatrix(matrixSize)
+  }
 
   const draw = (ctx: any, frameCount: number): void => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
@@ -53,8 +78,12 @@ function App() {
           Select matrix size: <input className="App-input" type="number" min="3" placeholder="3" />
         </div>
         <div>
-          <button className="App-button">Plot matrix</button>
-          <button className="App-button">process matrix</button>
+          <button className="App-button" onClick={getMatrix}>
+            Plot matrix
+          </button>
+          <button className="App-button" onClick={updateMatrix}>
+            process matrix
+          </button>
         </div>
 
         <Canvas draw={draw} width="300px" height="300px" />
