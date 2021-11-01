cab9eb3f8369e7085bdbf119437d18dae8137c47
Armando del Río <adrio.coder@gmail.com>
2021-11-01 10:08:09 -0600

    Finish project

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,32 +1,39 @@
 import logo from './logo.svg'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import { MatrixSize, MatrixValues } from './proto/blackPixels_pb'
+import { MatrixSize, MatrixValues, MatrixValue, NoParam } from './proto/blackPixels_pb'
 import { PixelServiceClient } from './proto/BlackPixelsServiceClientPb'
 import Canvas from './components/Canvas'
 
 function App() {
   const client = new PixelServiceClient('http://localhost:8080')
 
-  const [size, setSize] = useState<number>(3)
+  const [size, setSize] = useState<number>(0)
+  const [killValue, setKillValue] = useState<{ row: number; col: number; val: number }>()
+
+  const handleInputChange = (event: any) => {
+    setSize(event.target.value)
+  }
 
   const [matrix, setMatrix] = useState([
     [1, 0, 0],
     [0, 1, 0],
     [0, 0, 1]
   ])
-  // useEffect(() => {
-  //   const matrixSize = new MatrixSize()
-  //   matrixSize.setN(5)
-  //   matrixSize.setN(5)
-  //   const stream1 = client.getMatrix(matrixSize)
-  //   stream1.on('data', chunk => {
-  //     console.log(chunk)
-  //   })
-  //   stream1.on('end', () => {
-  //     console.log('aweso')
-  //   })
-  // }, [])
+
+  useEffect(() => {
+    const newMatrix = JSON.parse(JSON.stringify(matrix))
+    const row = killValue?.row
+    const col = killValue?.col
+
+    if (row && col) {
+      console.log('entra', row, col)
+      console.log(newMatrix[row][col])
+      newMatrix[row][col] = 0
+      // newMatrix[killValue.row][killValue.col]
+       setMatrix(newMatrix)
+    }
+  }, [killValue])
 
   const getMatrix = async () => {
     const theMatrix: any = []
@@ -36,7 +43,6 @@ function App() {
     const stream1 = client.getMatrix(matrixSize)
     stream1.on('data', chunk => {
       const msg = (chunk as MatrixValues).toObject()
-      console.log(msg.listList)
       theMatrix.push(msg.listList)
     })
     stream1.on('end', () => {
@@ -44,10 +50,15 @@ function App() {
     })
   }
   const updateMatrix = async () => {
-    // const matrixSize = new MatrixSize()
-    // matrixSize.setN(5)
-    // matrixSize.setN(5)
-    // const stream1 = client.getMatrix(matrixSize)
+    const stream2 = client.getNewMatrixValues(new NoParam())
+    let counter = 0
+
+    stream2.on('data', chunk => {
+      const { row, col, val } = (chunk as MatrixValue).toObject()
+      setKillValue({ row, col, val })
+    })
+
+    stream2.on('end', () => {})
   }
 
   const draw = (ctx: any, frameCount: number): void => {
@@ -75,13 +86,13 @@ function App() {
         </h1>
 
         <div>
-          Select matrix size: <input className="App-input" type="number" min="3" placeholder="3" />
+          Select matrix size: <input className="App-input" type="number" min="3" onChange={handleInputChange} placeholder="3" />
         </div>
         <div>
-          <button className="App-button" onClick={getMatrix}>
+          <button className="App-button" disabled={size < 3} onClick={getMatrix}>
             Plot matrix
           </button>
-          <button className="App-button" onClick={updateMatrix}>
+          <button className="App-button" disabled={size < 3} onClick={updateMatrix}>
             process matrix
           </button>
         </div>
